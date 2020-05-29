0bc24fddf548a6f8ae060ce5ee8fd19b9b3df343
Ilya Galkin <hardware.sitara@gmail.com>
2020-05-29 23:56:12 +0400

    new: Добавил оболочку на С для обработчиков прерываний

---
+++ firmware/application/inc/Interrupt.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+extern "C" {
+
+    void NMI_Handler (void);
+	void HardFault_Handler (void);
+	void MemManage_Handler (void);
+	void BusFault_Handler (void);
+	void UsageFault_Handler (void);
+	void SVC_Handler (void);
+	void DebugMon_Handler (void);
+	void PendSV_Handler (void);
+	void SysTick_Handler (void);
+
+    void TIM6_DAC1_IRQHandler (void);
+	void ADC1_2_IRQHandler (void);
+}
